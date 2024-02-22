1e66faa5ed58d1c3f202eaf7a555666eb90a8f44
Regisjr246 <regisjr246@gmail.com>
2024-02-22 09:28:52 -0300

    Apredendo React mobile

---
+++ src/login/LoginScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+
+
+function LoginScreen(): JSX.Element{
+    return (
+        <View>
+
+        </View>
+
+    );
+    
+}
+
+
+const Styles= StyleSheet.create({
+cotainer:{
+ 
+}
+});
+
+
+export default LoginScreen;
