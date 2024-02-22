a953ed120a73529b81aa15b0b92e7431478a4f4a
Regisjr246 <regisjr246@gmail.com>
2024-02-22 10:51:39 -0300

    apredendo alterações de layout

--- src/login/LoginScreen.tsx
+++ src/login/LoginScreen.tsx
@@ -1,33 +1,51 @@
 import React from 'react';
 import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
-function LoginScreen():JSX.Element{
-
-    return(
+function LoginScreen(): JSX.Element {
+    return (
         <View style={styles.container}>
-            <Image resizeMode='contain' source={require('../assets/images/user.png')} />
-        
-        
-        
-<View>
-<Text>Login</Text>
-<TextInput placeholder="E-mail" placeholderTextColor={"000000"}></TextInput>
-
-</View>
+            <Image style={styles.logo} resizeMode='contain' source={require('../assets/images/user.png')} />
+            <View style={styles.card}>
+                <Text style={styles.title}>Login</Text>
+                <TextInput placeholder="E-mail" placeholderTextColor={"#151413"}></TextInput>
+                <TextInput placeholder="Senha" placeholderTextColor={"#151413"}></TextInput>
+            </View>
         </View>
-
-
-
-
     );
 }
-
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#F76900'
+    },
+
+    logo: {
+        width: 80,
+        height: 80,
+        marginBottom: 20,
+        marginTop: -150
+    },
+
+    card: {
+        backgroundColor: "#FFFFFF",
+        width: 300,
+        borderRadius: 10,
+        padding: 20,
+        elevation: 3,
+        shadowColor: 'rgba(0,0,0,0.3)',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+    },
+
+    title:{
+        fontSize:25,
+        fontWeight:'bold',
+        color: '#151413',
+        marginBottom:20,
+        textAlign:'center',
     }
 });
 
